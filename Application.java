324d49e91cc756f89e3bab9d2391788e1c351527
Paul Friedli <paul.friedli@edufr.ch>
2026-08-27 17:44:53 +0200

    Initial commit

---
+++ Application.java
@@ -0,0 +1,40 @@
+public class Application {
+
+    // -----------------------------------------------------------
+    // Sujets : boucles (+sujets E0b)
+    // -----------------------------------------------------------
+
+    public static void main(String[] args) {
+
+        // Au bon endroit, déclarez une constante qui représente le tirage min au Swiss
+        // Loto (c'est la valeur 1)
+
+        // Au bon endroit, déclarez une constante qui représente le tirage max au Swiss
+        // Loto (c'est la valeur 42)
+
+        // Au bon endroit, déclarez une constante qui représente le nombre de grilles
+        // Swiss Loto a générer (par exemple 3)
+
+        // Déclarez une variable dans laquelle vous mettrez votre nombre entier fétiche
+        // (un qui vous plaît plus que les autres, genre le 7)
+
+        // Affichez "Générateur de tirages de Swiss Loto - by NOM Prénom" (le votre bien
+        // évidemment ;-)
+
+        // @formatter:off
+        //
+        // Répétez autant de fois qu'il y a de nombre de grilles de loto a générer
+        //      Affichez (sans retour à la ligne) "Voici les numéros Swiss Loto de la grille N°X : " (<== X sera 1, puis 2, ...)
+        //      Générez 6 numéros aléatoires entre les limites min max ci-dessus
+        //          Affichez-les suivis d'un espace, sans passer à la ligne suivante.
+        //          Et si le nombre a afficher est votre nombre fétiche affichez un astérisque à la place.
+        //      Sur la console, passez à la ligne suivante
+        //
+        // @formatter:on
+
+        // Comptez et affichez les nombres de 1 à 10. Mais si le nombre tombe sur votre
+        // nombre fétiche cessez immédiatement de compter.
+
+    }
+
+}
